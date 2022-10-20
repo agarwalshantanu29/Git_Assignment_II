a0d3d7e6043af01a8f2dc8245afa321bddec56ae
agarwalshantanu29 <99840256+agarwalshantanu29@users.noreply.github.com>
2022-10-20 13:07:45 +0530

    Update problem2.js

---
+++ problem2.js
@@ -0,0 +1,14 @@
+//check string is palindrome or not
+let str="abcd";
+for(let i=str.length-1;i>=0;i--)
+{
+bag+=str
+}
+if(bag==str)
+{
+console.log(str, "is a palindrome");
+}
+else
+{
+console.log(str, "is not a palindrome");
+}
